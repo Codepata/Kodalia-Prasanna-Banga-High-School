035f6604d5a50805b32a29563ff00ff74d30d42b
Tirthaboss <shrabanichakraborty83@gmail.com>
2026-09-15 14:26:18 +0530

    remove google site verification meta tag from page.tsx and add verification HTML file

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,7 @@ import React from 'react'
 export default function page() {
   return (
     <div>
-      <html>
-        <meta name="google-site-verification" content="bfAMrurrc4BbKdffU7ZZIJCxWFfRa_BowK1dwITUgKg" />
-      </html>
+      
       <Navbar />
     </div>
   )
