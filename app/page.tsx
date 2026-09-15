623cda56230bfd0e49593d09cb0af0b20b61d4b8
Tirthaboss <shrabanichakraborty83@gmail.com>
2026-09-15 13:53:13 +0530

    remove google site verification meta tag and associated HTML file; update Firebase configuration to include measurementId and initialize gtag

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,7 @@ import Navbar  from "@/components/ui/navbar";
 export default function page() {
   return (
     <>
-    <head>
-
-    <meta name="google-site-verification" content="bfAMrurrc4BbKdffU7ZZIJCxWFfRa_BowK1dwITUgKg" />
-    </head>
+    
     <div>
       <Navbar />
     </div>
