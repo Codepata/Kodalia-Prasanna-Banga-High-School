83725aa5df4adc7cf474f739444cee17091a8bc5
Tirthaboss <shrabanichakraborty83@gmail.com>
2026-09-15 13:29:44 +0530

    add google site verification meta tag

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,11 @@
-import React from 'react'
 import Navbar  from "@/components/ui/navbar";
 export default function page() {
   return (
     <>
+    <head>
+
+    <meta name="google-site-verification" content="bfAMrurrc4BbKdffU7ZZIJCxWFfRa_BowK1dwITUgKg" />
+    </head>
     <div>
       <Navbar />
     </div>
